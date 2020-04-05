624125710efbffa7bba234ab9340069bec3392b2
Heidi Tran <HeidiTran1410@gmail.com>
2020-04-05 17:21:49 -0400

    Hide About page, revert history changes

--- sudoku-solver/src/component/NavMenu.js
+++ sudoku-solver/src/component/NavMenu.js
@@ -17,8 +17,8 @@ export class NavMenuComponent extends React.Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/Sudoku-Solver">Home</Nav.Link>
+          {/* <Nav.Link href="/about">About</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- sudoku-solver/src/router/router.js
+++ sudoku-solver/src/router/router.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import * as createHistory from 'history';
 import { NavMenuComponent } from '../component/NavMenu';
 import { HomeComponent } from '../component/Home';
-import { AboutComponent } from '../component/About';
+// import { AboutComponent } from '../component/About';
+
+export const history = createHistory.createBrowserHistory();
 
 export const AppRouter = () => (
-  <Router>
+  <Router history={ history }>
     <NavMenuComponent />
     <Container fluid className="mt-3">
       <Switch>
-        <Route path='/Sudoku-Solver/home' component={ HomeComponent }/>
-        <Route path='/Sudoku-Solver/about' component={ AboutComponent }/>
-        <Redirect from='/Sudoku-Solver' to='/Sudoku-Solver/home' />
+        <Route path='/Sudoku-Solver' component={ HomeComponent }/>
+        {/* <Route path='/Sudoku-Solver/about' component={ AboutComponent }/>
+        <Redirect from='/Sudoku-Solver' to='/Sudoku-Solver/home' /> */}
       </Switch>
     </Container>
   </Router>

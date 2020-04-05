0b2d0823a7f41ca3c174c4955bf5d5410aba0fbd
st974 <st974@drexel.edu>
2020-04-04 20:52:30 -0400

    Fixed some styling issues related to the footer

--- sudoku-solver/src/component/Footer.js
+++ sudoku-solver/src/component/Footer.js
@@ -6,7 +6,7 @@ import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 export class FooterComponent extends React.Component {
   render() {
     return (
-      <Navbar expand="lg" variant="light" bg="light" sticky="bottom" fixed="bottom">
+      <Navbar expand="lg" variant="light" bg="light">
         <Navbar.Collapse className="justify-content-center">
           <Navbar.Text>
             Made with &hearts; by Heidi Tran and Sammy Tran
